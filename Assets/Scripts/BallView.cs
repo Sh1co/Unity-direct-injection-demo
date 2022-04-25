d65567225f3b797b2ca9c1ea812ad2307f8aff35
Sherif Nafee <shico.m.nafee@gmail.com>
2022-04-25 22:39:14 +0300

    Add original code

---
+++ Assets/Scripts/BallView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+
+public class BallView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _healthText;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.transform.TryGetComponent(out BoxTag _)) return;
+        CollidedWithBox?.Invoke();
+    }
+
+
+    public void DisplayHealth(int health)
+    {
+        _healthText.text = health.ToString();
+    }
+
+    public event Action CollidedWithBox;
+}
